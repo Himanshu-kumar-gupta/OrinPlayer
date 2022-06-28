eae940ed0eb7c86f7d1466800a598503b49386b2
Himanshu-kumar-gupta <himanshu63936gupta@gmail.com>
2022-06-29 02:11:41 +0530

    Added Storage permission

--- app/src/main/java/com/HimanshuKumarGupta/orinplayer/MainActivity.kt
+++ app/src/main/java/com/HimanshuKumarGupta/orinplayer/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.HimanshuKumarGupta.orinplayer
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.HimanshuKumarGupta.orinplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_OrinPlayer)
+        requestRuntimePermissions()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -25,6 +29,27 @@ class MainActivity : AppCompatActivity() {
 
         binding.playlistBtn.setOnClickListener {
             startActivity(Intent(this@MainActivity, playlistActivity::class.java))
+        }
+
+    }
+
+    private fun requestRuntimePermissions(){
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),11)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode==11){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission is Granted", Toast.LENGTH_SHORT).show()
+            else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
