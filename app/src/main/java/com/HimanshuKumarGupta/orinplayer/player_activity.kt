231a53414635c7f2c8920901edfeed692276011f
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2022-07-03 14:47:30 +0530

    Added functionality of PLAY/PAUSE and songImage,songText in Player activity

--- app/src/main/java/com/HimanshuKumarGupta/orinplayer/player_activity.kt
+++ app/src/main/java/com/HimanshuKumarGupta/orinplayer/player_activity.kt
@@ -4,36 +4,66 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.HimanshuKumarGupta.orinplayer.databinding.ActivityPlayerBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class player_activity : AppCompatActivity() {
-
     private lateinit var binding: ActivityPlayerBinding
 
     companion object {
         lateinit var musicListPA : ArrayList<Music>
         var songPosition: Int = 0
-        lateinit var mediaPlayer : MediaPlayer
+        var mediaPlayer : MediaPlayer? = null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.coolActivity)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_player)
+        setContentView(binding.root)
+        setLayout()
+        binding.playPauseBtn.setOnClickListener {
+            if (mediaPlayer!!.isPlaying) pauseMusic()
+            else playMusic()
+        }
+    }
 
+    private fun setLayout() {
         songPosition = intent.getIntExtra("index", 0)
         when(intent.getStringExtra("class")) {
             "MusicAdapter" -> {
                 //Creating a reference to music list in main activity
                 musicListPA = MainActivity.musicListMA
-                mediaPlayer = MediaPlayer()
-                mediaPlayer.reset()
-                mediaPlayer.setDataSource(musicListPA[songPosition].path)
-                mediaPlayer.prepare()
-                mediaPlayer.start()
-
+                setImageText()
+                createMediaPlayer()
             }
-
         }
+    }
+
+    private fun setImageText() {
+        Glide.with(this)
+            .load(musicListPA[songPosition].artUri)
+            .apply(RequestOptions().placeholder(R.drawable.app_icon).centerCrop())
+            .into(binding.songImagePA)
+
+        binding.songNamePA.text = musicListPA[songPosition].songNameM
+    }
+
+    private fun createMediaPlayer() {
+        if(mediaPlayer == null) mediaPlayer = MediaPlayer()
+        mediaPlayer!!.reset()
+        mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
+        mediaPlayer!!.prepare()
+        mediaPlayer!!.start()
+    }
+
+    private fun playMusic() {
+        binding.playPauseBtn.setIconResource(R.drawable.pause_button)
+        mediaPlayer!!.start()
+    }
+
+    private fun pauseMusic() {
+        binding.playPauseBtn.setIconResource(R.drawable.play_btn)
+        mediaPlayer!!.pause()
     }
 }
